3523e483f5bcc822360c2e1867cb1ad37934f499
IgorEverton <rm95214@fiap.com.br>
2022-09-30 20:22:02 -0300

    Estrutura do Form do pet criado

--- app-pet/src/components/petCadastro/PetCadastro.jsx
+++ app-pet/src/components/petCadastro/PetCadastro.jsx
@@ -3,7 +3,43 @@ import React from "react";
 export default function PetCadastro(){
     return(
         <div>
-            <h1>Formulário</h1>
-        </div>
+        <form method="post" onSubmit={props.addTarefa}>
+            <div>
+                <label>Nome do seu Pet</label>
+                <input type="text" name="nomePet" value/>
+            </div>
+            <div>
+                <label>Idade do seu Pet</label>
+                <input type="number" name="idade" value/>
+            </div>
+            <div>
+                <label>Raça do seu Pet</label>
+                <input type="text" name="raca" value/>
+            </div>
+            <div>
+                <label>Tamanho do seu Pet</label>
+                <input type="number" name="tamanho" value/>
+            </div>
+            <div>
+                <label>Nome do dono do Pet</label>
+                <input type="text" name="nomeDono" value/>
+            </div>
+            <div>
+                <label>Telefone do dono do Pet</label>
+                <input type="tel" name="telDono" value/>
+            </div>
+            <div>
+                <label>Foto do seu Pet</label>
+                <input type="imt" name="imgPet" value/>
+            </div>
+            <div>
+                <label>Oberservações</label>
+                <textarea name="obs" cols="30" rows="10" value={} onChange={}></textarea>
+            </div>
+            <div>
+                <button type="submit">Adicionar</button>
+            </div>
+        </form>
+    </div>
     )
 }
