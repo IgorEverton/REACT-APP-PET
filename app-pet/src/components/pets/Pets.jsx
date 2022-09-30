7639a6702d81de55f4929ce24046a4b72f00e304
IgorEverton <rm95214@fiap.com.br>
2022-09-30 20:32:56 -0300

    criando useStates no Pets

--- app-pet/src/components/pets/Pets.jsx
+++ app-pet/src/components/pets/Pets.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Pets(){
+    
+    const [pets, setPets] = useState ([
+        {"nome":"", "idade": "", "raca": "", "tamanho": "", "nomeDono": "", "telDono": "", "img": "", "obs": ""}
+    ])
+
+    
     return(
         <div>
             <h1>Pets Cadastrados</h1>
