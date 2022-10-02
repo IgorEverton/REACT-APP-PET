6fe4f508fbbee29dd1dd8da8bb5894e65e12242d
andmaia <andrewrian198@gmail.com>
2022-10-01 21:03:48 -0300

    Ajeitando bus no formulário de pets

--- app-pet/src/components/petCadastro/PetCadastro.jsx
+++ app-pet/src/components/petCadastro/PetCadastro.jsx
@@ -9,31 +9,31 @@ export default function PetCadastro(props) {
                 <form method="post" onSubmit={props.addPets}>
                     <div>
                         <label>Nome do seu Pet</label>
-                        <input type="text" name="nomePet" value />
+                        <input type="text" name="nomePet" value='' />
                     </div>
                     <div>
                         <label>Idade do seu Pet</label>
-                        <input type="number" name="idade" value />
+                        <input type="number" name="idade" value="" />
                     </div>
                     <div>
                         <label>Raça do seu Pet</label>
-                        <input type="text" name="raca" value />
+                        <input type="text" name="raca" value="" />
                     </div>
                     <div>
                         <label>Tamanho do seu Pet</label>
-                        <input type="number" name="tamanho" value />
+                        <input type="number" name="tamanho" value="" />
                     </div>
                     <div>
                         <label>Nome do dono do Pet</label>
-                        <input type="text" name="nomeDono" value />
+                        <input type="text" name="nomeDono" value="" />
                     </div>
                     <div>
                         <label>Telefone do dono do Pet</label>
-                        <input type="tel" name="telDono" value />
+                        <input type="tel" name="telDono" value="" />
                     </div>
                     <div>
                         <label>Foto do seu Pet</label>
-                        <input type="imt" name="imgPet" value />
+                        <input type="imt" name="imgPet" value="" />
                     </div>
                     <div>
                         <label>Oberservações</label>
